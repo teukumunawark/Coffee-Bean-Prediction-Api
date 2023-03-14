77789cc73878d5669271db95b8be96072c4af6b6
teukumunawark <munawartmk@gmail.com>
2023-03-14 20:20:57 +0700

    update

--- app.py
+++ app.py
@@ -1,9 +1,9 @@
 import os
 from werkzeug.utils import secure_filename
-from deep_learnig import prediction, multiprediction
-from flask import Flask, request, jsonify
-from backgound_check import backgound_detected
-
+from camera import gen_frames
+from deep_learnig import single_prediction, multiprediction
+from flask import Flask, request, jsonify, Response
+from backgound_check import single_bg_detected
 
 
 
@@ -20,7 +20,8 @@ def allowed_file(filename):
            filename.rsplit('.', 1)[1].lower(
            ) in app.config['ALLOWED_EXTENSIONS']
 
-@app.route('/predictions', methods=['POST'])
+
+@app.route('/singleprediction', methods=['POST'])
 def single_processing():
     if 'file' not in request.files:
         return {'error': 'No file in request'}, 400
@@ -35,10 +36,10 @@ def single_processing():
 
         file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
 
-        detected_respone = backgound_detected('./uploads/' + filename)
+        detected_respone = single_bg_detected('./uploads/' + filename)
 
         if detected_respone != False:
-            prediction_response = prediction('./uploads/' + filename)
+            prediction_response = single_prediction('./uploads/' + filename)
             os.remove('./uploads/' + filename)
 
             return prediction_response, 200
@@ -52,16 +53,25 @@ def single_processing():
 
 @app.route('/multipredictions', methods=['POST'])
 def multiple_processing():
+    if 'file' not in request.files:
+        return {'error': 'No file in request'}, 400
+    file = request.files['file']
+
+    if file.filename == '':
+        return {'error': 'No file selected'}, 400
+
     uploaded_files = request.files.getlist('file')
+
     filenames = []
+
     for file in uploaded_files:
         filename = secure_filename(file.filename)
         file_path = os.path.join(app.config['UPLOAD_FOLDER'], filename)
         file.save(file_path)
         filenames.append(file_path)
-        
+
     response = multiprediction(filenames)
-    
+
     # Remove uploaded files after processing
     for filename in filenames:
         print(f"Deleting file: {filename}")
@@ -69,8 +79,10 @@ def multiple_processing():
             os.remove(filename)
         else:
             print(f"File not found: {filename}")
-    
+
     return response
 
 
-    
+@app.route('/camera')
+def video_feed():
+    return Response(gen_frames(), mimetype='multipart/x-mixed-replace; boundary=frame')
