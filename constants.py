d3fb13afff5d90436d0a08b66ead19d48a32cfea
teukumunawark <munawartmk@gmail.com>
2023-04-05 17:48:36 +0700

    adding constant variable

---
+++ constants.py
@@ -0,0 +1,23 @@
+import torch
+from torchvision import datasets, transforms
+
+batch_size = 4
+train_dir = './data/train'
+
+mean = [0.7980, 0.7820, 0.7562]
+std = [0.1441, 0.1729, 0.2341]
+
+# Load the model from file
+model = torch.load('./models/model.pth')
+model.eval()
+
+if torch.cuda.is_available():
+    model.cuda()
+
+idx_to_class = {v: k for k, v in datasets.ImageFolder(
+    root=train_dir).class_to_idx.items()}
+
+image_transforms = transforms.Compose([
+    transforms.ToTensor(),
+    transforms.Normalize(mean, std)
+])
