b929bf85ac1c3a02e5658d3266147476a3599dfb
teukumunawark <munawartmk@gmail.com>
2023-06-02 23:23:30 +0700

    refactor app file

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 import os
 from werkzeug.utils import secure_filename
-from deep_learning.single_predict import singleprediction
-from deep_learning.multi_predict import multiprediction
+from deep_learning.single_classification import singleclassification, highestscore
+from deep_learning.multi_classification import multiclassification
 from flask import Flask, request
 from utils.backgound_check import single_bg_detected
 
@@ -19,58 +19,76 @@ def allowed_file(filename):
            filename.rsplit('.', 1)[1].lower(
            ) in app.config['ALLOWED_EXTENSIONS']
 
-@app.route('/singleprediction', methods=['POST'])
+
+def check_file(file):
+    if file is None:
+        return {'error': 'No file in request'}, 400
+
+    if file.filename == '':
+        return {'error': 'No file selected'}, 400
+
+    if not allowed_file(file.filename):
+        return {'error': 'File type not allowed'}, 400
+
+    return file
+
+
+@app.route('/singleclassification', methods=['POST'])
 def single_processing():
-    if 'file' not in request.files:
-        return {'error': 'No file in request'}, 400
-    file = request.files['file']
+    file = request.files['image']
+    file = check_file(file)
 
-    if file.filename == '':
-        return {'error': 'No file selected'}, 400
+    filename = secure_filename(file.filename)
+    file_path = os.path.join(app.config['UPLOAD_FOLDER'], filename)
+    file.save(file_path)
 
-    if file and allowed_file(file.filename):
-        filename = secure_filename(file.filename)
-        file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
-        detected_respone = single_bg_detected('./uploads/' + filename)
+    detected_respone = single_bg_detected(file_path)
 
-        if detected_respone != False:
-            prediction_response = singleprediction('./uploads/' + filename)
-            os.remove('./uploads/' + filename)
-            return prediction_response, 200
+    if detected_respone != False:
+        prediction_response = singleclassification(file_path)
+        os.remove(file_path)
+        return prediction_response, 200
 
-        os.remove('./uploads/' + filename)
+    os.remove(file_path)
 
-        return {'message': 'process is rejected'}, 204
-    else:
-        return {}, 404
+    return {'message': 'process is rejected'}, 204
 
 
-@app.route('/multipredictions', methods=['POST'])
+@app.route('/highestscore', methods=['POST'])
+def highest_score():
+    file = request.files['image']
+    file = check_file(file)
+
+    filename = secure_filename(file.filename)
+    file_path = os.path.join(app.config['UPLOAD_FOLDER'], filename)
+    file.save(file_path)
+
+    prediction_response = highestscore(file_path)
+    os.remove(file_path)
+    return prediction_response, 200
+
+
+@app.route('/multiclassifications', methods=['POST'])
 def multiple_processing():
-    if 'file' not in request.files:
-        return {'error': 'No file in request'}, 400
-    file = request.files['file']
+    uploaded_files = request.files.getlist('image')
 
-    if file.filename == '':
-        return {'error': 'No file selected'}, 400
-
-    uploaded_files = request.files.getlist('file')
+    if not uploaded_files:
+        return {'error': 'No files uploaded'}, 400
 
     filenames = []
-
     for file in uploaded_files:
+        file = check_file(file)
         filename = secure_filename(file.filename)
         file_path = os.path.join(app.config['UPLOAD_FOLDER'], filename)
         file.save(file_path)
         filenames.append(file_path)
 
-    response = multiprediction(filenames)
+    response = multiclassification(filenames)
 
     for filename in filenames:
-        print(f"Deleting file: {filename}")
-        if os.path.exists(filename):
+        try:
             os.remove(filename)
-        else:
+        except FileNotFoundError:
             print(f"File not found: {filename}")
 
     return response
